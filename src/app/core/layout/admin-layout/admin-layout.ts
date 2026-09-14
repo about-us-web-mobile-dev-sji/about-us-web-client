d93132ce8b9ed26b469a4c39cff6ea48a85263bf
off1805 <ouafofotsoh@gmail.com>
2026-09-14 13:46:33 +0100

    fix: integration des pages de la gestion des ecoles

--- src/app/core/layout/admin-layout/admin-layout.ts
+++ src/app/core/layout/admin-layout/admin-layout.ts
@@ -29,6 +29,7 @@ export class AdminLayout {
       items: [
         { icon: 'home', label: 'Accueil', route: '/s/home' },
         { icon: 'users', label: 'Utilisateurs', route: '/s/users' },
+        { icon: 'users', label: 'Ecoles', route: '/s/schools/create' },
         { icon: 'history', label: 'Évènements', route: '/s/events' },
 
         
