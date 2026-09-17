6a0b7bacc7271afa6ecda4e461522ab866ff98a5
NGUEDIA <nellucreceesther@gmail.com>
2026-09-17 19:09:42 +0100

    besoin de commiter mon travail pour telecharger une dependance

---
+++ src/app/popup/popup.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-popup',
+  styleUrl: './popup.css',
+  templateUrl: './popup.html',
+})
+export class Popup {}
